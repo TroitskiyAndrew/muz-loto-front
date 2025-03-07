4ff63721ab8eca7342bd1a05e5b493587c2f8689
AndreyTroitskiy <TroitskiyA@yandex.ru>
2025-03-07 10:00:41 +0530

    Revert "feat: new back"
    This reverts commit 18472898778f6e304d7000b5ef8c5ea57950b0e0.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   videoStep: 5,
   videoStepDuration: 100,
   backgroundVolume: 30,
-  backendUrl: 'http://5.101.179.132:3999'
+  backendUrl: 'https://cherepavel.com/muzloto'
 };
